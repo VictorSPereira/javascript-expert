ea4cca36243012696aaa8d28d6655bc4952a5b60
vitorpereira-01@hotmail.com <vitorpereira-01@hotmail.com>
2022-03-16 08:22:19 -0300

    config

---
+++ server/config.js
@@ -0,0 +1,8 @@
+import {join, dirname} from 'path'
+import { fileURLToPath } from 'url'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+const root = join( currentDir, '../')
+const audioDirectory = join( root, 'audio')
+const publicDirectory = join( root, 'public')
+
